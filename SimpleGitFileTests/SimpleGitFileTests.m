69e8a4670e44f738092e352daef6190e0fecb1cd
Sravani <krishnareddykovvuri@Sravanis-Mac-mini.local>
2014-04-01 17:58:37 +0530

    Initial Commit

---
+++ SimpleGitFileTests/SimpleGitFileTests.m
@@ -0,0 +1,34 @@
+//
+//  SimpleGitFileTests.m
+//  SimpleGitFileTests
+//
+//  Created by Sravani on 01/04/14.
+//  Copyright (c) 2014 Krify. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleGitFileTests : XCTestCase
+
+@end
+
+@implementation SimpleGitFileTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
